59c5001bdac9775c209a5092b9ddfd5702497538
Dani <dkralj@gmail.com>
2023-03-25 16:22:48 +0100

    add 8.2 as tag

--- build_next.sh
+++ build_next.sh
@@ -17,11 +17,13 @@ docker manifest rm vemcogroup/php-cli:${TAG} &
 wait
 
 docker manifest create vemcogroup/php-cli:next --amend vemcogroup/php-cli:next-amd64 --amend vemcogroup/php-cli:next-arm64 &
+docker manifest create vemcogroup/php-cli:8.2 --amend vemcogroup/php-cli:next-amd64 --amend vemcogroup/php-cli:next-arm64 &
 docker manifest create vemcogroup/php-cli:${TAG} --amend vemcogroup/php-cli:next-amd64 --amend vemcogroup/php-cli:next-arm64 &
 
 wait
 
 docker manifest push vemcogroup/php-cli:next &
+docker manifest push vemcogroup/php-cli:8.2 &
 docker manifest push vemcogroup/php-cli:${TAG} &
 
 wait
